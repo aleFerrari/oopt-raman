65ab9e061904cb63b29e4532aff746e220fde9f9
Alessio Ferrari <alessio.ferrari@polito.it>
2018-08-04 12:03:18 +0200

    tests have been added both for raman and for utilities

---
+++ tests/utilities_test.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: Alessio Ferrari
+"""
+import math as mt
+import numpy as np
+import pytest
+import raman.utilities as ut
+from numpy import testing as npt
+from collections import namedtuple
+from operator import attrgetter
+
+
+@pytest.mark.parametrize("num_channels", [1, 10])
+def test_compute_power_spectrum(num_channels):
+
+    delta_f = 50e9
+    pch = 1e-3
+    roll_off = 0.1
+    symbol_rate = 32e9
+    start_f = 191.0e12
+    wdm_band = num_channels * delta_f
+    stop_f = start_f + wdm_band
+    frequency_slice_size = 50e9
+
+    pump_pow = [0.450, 0.400]
+    pump_freq = [204.0e12, 206.3e12]
+    pump_direction = [-1, -1]
+    num_pumps = len(pump_pow)
+
+    spectral_information = namedtuple('SpectralInformation', 'carriers')
+    raman_pump_information = namedtuple('SpectralInformation', 'raman_pumps')
+    channel = namedtuple('Channel', 'channel_number frequency baud_rate roll_off power')
+    power = namedtuple('Power', 'signal nonlinear_interference amplified_spontaneous_emission')
+    pump = namedtuple('RamanPump', 'pump_number power frequency propagation_direction')
+
+    carriers = tuple(channel(1 + ii, start_f + (delta_f * ii), symbol_rate, roll_off, power(pch, 0, 0))
+                                     for ii in range(0, num_channels))
+    pumps = tuple(pump(1 + ii, pump_pow[ii], pump_freq[ii], pump_direction[ii])
+                                for ii in range(0, num_pumps))
+    spec_info = spectral_information(carriers=carriers)
+    raman_pump_info = raman_pump_information(raman_pumps=pumps)
+
+    pow_array, f_array, propagation_direction = ut.compute_power_spectrum(spec_info, raman_pump_info)
+
+    # Computing expected values for wdm channels
+    n_slices = mt.ceil(wdm_band / frequency_slice_size)
+    pow_slice = pch * frequency_slice_size / delta_f
+    pow_last_slice = (wdm_band / frequency_slice_size) % 1 * pow_slice
+
+    pow_array_test = np.ones(n_slices) * pow_slice
+    if pow_last_slice:
+        pow_array_test[-1] = pow_last_slice
+
+    f_array_test = np.arange(start_f, stop_f, frequency_slice_size)
+
+    propagation_direction_test = np.ones(num_channels)
+
+    # Computing expected values channels + Raman pumps
+    pow_array_test = np.append(pow_array_test, pump_pow)
+    f_array_test = np.append(f_array_test, pump_freq)
+    propagation_direction_test = np.append(propagation_direction_test, pump_direction)
+
+    npt.assert_allclose(pow_array_test, pow_array, rtol=1e-6)
+    npt.assert_allclose(f_array_test, f_array, rtol=1e-6)
+    npt.assert_allclose(propagation_direction_test, propagation_direction, rtol=1e-6)
+
