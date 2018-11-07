100789ed7bb8c0287fd47983daa96e45c61288df
AndreaDAmico <andrea.damico.dap@gmail.com>
2018-11-07 18:43:25 +0100

    rescaling units

--- examples/main_compute_nli.py
+++ examples/main_compute_nli.py
@@ -117,12 +117,12 @@ if __name__ == '__main__':
 
     csv_files_dir = './resources/'
 
-    f_axis = (1E-12)*np.loadtxt(open(csv_files_dir+'f_axis.csv','rb'),delimiter=',')
-    z_array = (1E-3)*np.loadtxt(open(csv_files_dir+'z_array.csv','rb'),delimiter=',')
+    f_axis = (1E+12)*np.loadtxt(open(csv_files_dir+'f_axis.csv','rb'),delimiter=',')
+    z_array = (1E+3)*np.loadtxt(open(csv_files_dir+'z_array.csv','rb'),delimiter=',')
     rho = np.loadtxt(open(csv_files_dir+'raman_profile.csv'),delimiter=',')
 
     guard_band_indices = range(78, 83)
-    f_channel = np.delete((1E-12) * np.loadtxt(open(csv_files_dir+'f_channel.csv', 'rb'), delimiter=','),guard_band_indices)
+    f_channel = np.delete((1E+12) * np.loadtxt(open(csv_files_dir+'f_channel.csv', 'rb'), delimiter=','),guard_band_indices)
     pch = 0.50119E-03*np.ones(len(f_channel))
     channel_numbers = range(len(f_channel))
 
