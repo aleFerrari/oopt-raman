784fe7b2f309797330af244fbae700e4c42d4a0d
AndreaDAmico <andrea.damico.dap@gmail.com>
2018-11-19 16:04:08 +0100

    ten channels

--- examples/main_compute_nli.py
+++ examples/main_compute_nli.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from operator import attrgetter
 from scipy.interpolate import interp1d
 
-cut_index = range(11);
+cut_index = [x for x in range(10,21)];
 
 def raman_gain_efficiency_from_csv(csv_file_name):
     with open(csv_file_name) as csv_file:
@@ -117,18 +117,15 @@ if __name__ == '__main__':
 
     csv_files_dir = './resources/'
 
-    f_axis = (1E+3)*np.loadtxt(open(csv_files_dir+'f_axis.csv','rb'),delimiter=',')
+    f_axis = (1E+12)*np.loadtxt(open(csv_files_dir+'f_axis.csv','rb'),delimiter=',')
     z_array = (1E+3)*np.loadtxt(open(csv_files_dir+'z_array.csv','rb'),delimiter=',')
     rho = np.loadtxt(open(csv_files_dir+'raman_profile.csv'),delimiter=',')
-    rho = rho[range(len(f_axis))]
     A = np.exp((-attenuation_coefficient_p / 2) * z_array)
     for i in range(len(rho)):
         rho[i] = np.multiply(rho[i], A)
 
     guard_band_indices = range(78, 83)
-    f_channel = np.array(range(18770,18820,5))/100
-
-
+    f_channel = f_axis[cut_index]
     pch = 0.50119E-03*np.ones(len(f_channel))
     channel_numbers = range(len(f_channel))
 
