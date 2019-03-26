d2fbef6daa4e7540ee7d32d945ec469862034279
Alessio Ferrari <alessio.ferrari@polito.it>
2019-03-26 16:18:03 +0100

    new test GN vs GGN_no_raman

--- tests/nli_test.py
+++ tests/nli_test.py
@@ -6,6 +6,7 @@ import numpy as np
 from numpy import testing as npt
 import pytest
 from raman import nli
+from raman import raman as rm
 from collections import namedtuple
 from scipy.interpolate import interp1d
 
@@ -85,7 +86,104 @@ def test_nli_fwm_efficiency(delta_beta, x_talk, delta_rho_fun):
 
     npt.assert_allclose(calculed, expected, rtol=1E-5)
 
+def test_disaggregated_gn_vs_ggn():
+        # FIBER PARAMETERS
+        cr = 0
+        frequency_cr = 0
 
+        fiber_length = np.array([75e3])
+        attenuation_coefficient_p = np.array([0.046e-3])
+        frequency_attenuation = np.array([193.5e12])
+
+        gamma = 1.27e-3  # 1/W/m
+        beta2 = 21.27e-27  # s^2/m
+        beta3 = 0.0344e-39  # s^3/m
+        f_ref_beta = 19.35e12  # Hz
+
+        # WDM COMB PARAMETERS
+        num_channels = 11
+        delta_f = 50e9
+        pch = 0.5e-3
+
+        cut_list = range(5, num_channels, 5)
+        cut_list = [45]
+
+        roll_off = 0.1
+        symbol_rate = 32e9
+        start_f = 191.0e12
+
+        # ODE SOLVER PARAMETERS
+        z_resolution = 1e3
+        tolerance = 1e-8
+        verbose_raman = 2
+
+        # NLI PARAMETERS
+        f_resolution_nli = 0.2e9
+        verbose_nli = 1
+        method_nli = 'GGN_spectrally_separated_spm_xpm'
+        n_points_per_slot_min = 10
+        n_points_per_slot_max = 5000
+        delta_f = 50e9
+        min_fwm_inv = 60
+        dense_regime = namedtuple('DenseRegimeParameters',
+                                  'n_points_per_slot_min n_points_per_slot_max delta_f min_fwm_inv')
+        dense_regime = dense_regime(n_points_per_slot_min=n_points_per_slot_min,
+                                    n_points_per_slot_max=n_points_per_slot_max, delta_f=delta_f,
+                                    min_fwm_inv=min_fwm_inv)
+
+        # FIBER
+        fiber_info = namedtuple('FiberInformation', 'length attenuation_coefficient raman_coefficient '
+                                                    'beta2 beta3 f_ref_beta gamma')
+        attenuation_coefficient = namedtuple('AttenuationCoefficient', 'alpha_power frequency')
+        raman_coefficient = namedtuple('RamanCoefficient', 'cr frequency')
+
+        att_coeff = attenuation_coefficient(alpha_power=attenuation_coefficient_p, frequency=frequency_attenuation)
+        raman_coeff = raman_coefficient(cr=cr, frequency=frequency_cr)
+        fiber = fiber_info(length=fiber_length, attenuation_coefficient=att_coeff, raman_coefficient=raman_coeff,
+                           gamma=gamma, beta2=beta2, beta3=beta3, f_ref_beta=f_ref_beta)
+
+        # SPECTRUM
+        spectral_information = namedtuple('SpectralInformation', 'carriers')
+        channel = namedtuple('Channel', 'channel_number frequency baud_rate roll_off power')
+        power = namedtuple('Power', 'signal nonlinear_interference amplified_spontaneous_emission')
+
+        carriers = tuple(channel(1 + ii, start_f + (delta_f * ii), symbol_rate, roll_off, power(pch, 0, 0))
+                         for ii in range(0, num_channels))
+        spectrum = spectral_information(carriers=carriers)
+
+        # SOLVER PARAMETERS
+        raman_solver_information = namedtuple('RamanSolverInformation', ' z_resolution tolerance verbose')
+        solver_parameters = raman_solver_information(z_resolution=z_resolution,
+                                                     tolerance=tolerance, verbose=verbose_raman)
+
+        # NLI PARAMETERS
+        nli_parameters = namedtuple('NLIParameters', 'method frequency_resolution verbose dense_regime')
+        model_params = nli_parameters(method=method_nli, frequency_resolution=f_resolution_nli,
+                                      verbose=verbose_nli, dense_regime=dense_regime)
+
+        raman_solver = rm.RamanSolver(fiber)
+        raman_solver.spectral_information = spectrum
+        raman_solver.solver_params = solver_parameters
+
+        nlint = nli.NLI(fiber_information=fiber)
+        nlint.srs_profile = raman_solver
+        nlint.model_parameters = model_params
+
+        carriers_nli_ggn = [nlint.compute_nli(carrier, *spectrum.carriers)
+                        for carrier in spectrum.carriers
+                        if carrier.channel_number in cut_list]
+
+        method_nli = 'GN_spectrally_separated_spm_xpm'
+        nli_parameters = namedtuple('NLIParameters', 'method frequency_resolution verbose dense_regime')
+        model_params = nli_parameters(method=method_nli, frequency_resolution=f_resolution_nli,
+                                      verbose=verbose_nli, dense_regime=dense_regime)
+        nlint.model_parameters = model_params
+
+        carriers_nli_gn = [nlint.compute_nli(carrier, *spectrum.carriers)
+                        for carrier in spectrum.carriers
+                        if carrier.channel_number in cut_list]
+        npt.assert_allclose(carriers_nli_ggn, carriers_nli_gn, rtol=1E-12)
+#
 # def test_nli_spectral_separated():
 #     # FIBER PARAMETERS
 #     fiber_information = namedtuple('FiberInformation',
@@ -95,6 +193,7 @@ def test_nli_fwm_efficiency(delta_beta, x_talk, delta_rho_fun):
 #     fiber_information.attenuation_coefficient = attenuation_coefficient_p
 #     fiber_information.beta2 = -21.27e-27  # s^2/m
 #     fiber_information.beta3 = 0  # s^3/m
+#     fiber_information.f_ref_beta = 0  # Hz
 #     fiber_information.gamma = 1.3e-3  # 1/W/m
 #
 #     model_parameters = namedtuple('NLIParameters', 'method frequency_resolution verbose')
